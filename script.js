1ddb71daa342930f5aa8e5c7cbedea9a95ffb791
RockyTechno <rockyraj4488@gmail.com>
2025-07-08 14:47:53 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById("contact-form")?.addEventListener("submit", function (e) {
-  e.preventDefault();
+ 
   document.getElementById("form-message").textContent = "Thanks for contacting us! We'll get back to you soon.";
   this.reset();
 });
